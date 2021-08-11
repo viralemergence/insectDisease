5a95c61ca9420e933a0cbc859f114c8eda9dd09c
taddallas <tad.a.dallas@gmail.com>
2021-08-11 15:28:06 -0500

    trying to get vigneette to work

---
+++ R/hostTaxonomy.R
@@ -0,0 +1,23 @@
+#' Cached version of host taxonomy (see vignette for code to build from scratch)
+#'
+#' 
+#' 
+#' 
+#'
+#' \itemize{
+#'   \item HostOriginal original host name from EDWIP
+#'   \item HostTaxID NCBI taxonomic ID
+#'   \item HostNCBIResolved is the host found in NCBI
+#'   \item HostGenus host genus
+#'   \item HostFamily host family
+#'   \item HostOrder host order
+#'   \item HostClass  host class
+#'  }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name hostTaxonomy
+#' @usage data(hostTaxonomy)
+#' @format A data.frame 
+
+"hostTaxonomy"
---
+++ R/pathTaxonomy.R
@@ -0,0 +1,20 @@
+#' Cached version of pathogen taxonomy (see vignette for code to build from scratch)
+#' 
+#'
+#' \itemize{
+#'   \item PathTaxID NCBI taxonomic ID
+#'   \item PathNCBIResolved is the pathogen found in NCBI
+#'   \item PathGenus pathogen genus
+#'   \item PathFamily pathogen family
+#'   \item PathOrder pathogen order
+#'   \item PathClass  pathogen class
+#'   \item PathKingdom  pathogen class
+#'  }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name pathTaxonomy
+#' @usage data(pathTaxonomy)
+#' @format A data.frame 
+
+"pathTaxonomy"
